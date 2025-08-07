5edce4c34a7eb0dae677e2f1a8feca9cde1a51db
kpizarro-ux <kpizarro@me.com>
2025-08-07 10:36:32 -0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TrackFinder from './components/TrackFinder';
+
+export default function App() {
+  return (
+    <div className="min-h-screen p-6 bg-gray-950 text-white">
+      <TrackFinder />
+    </div>
+  );
+}
