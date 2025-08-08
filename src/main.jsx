985851029be11d4360a2220f473ecd4dc9e84339
Karlo Pizarro <kpizarro@me.com>
2025-08-08 10:36:15 -0700

    Fix: Resolve merge conflict in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> 794855189e1086e45907bbf6a25490357d4186a7
